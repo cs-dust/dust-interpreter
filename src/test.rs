f2fcd3fa56591f58b0975431e9a2d5a2bc8655b5
sevenseasofbri <vishruti250102@gmail.com>
2023-03-25 17:03:00 +0800

    now restore env

--- src/test.rs
+++ src/test.rs
@@ -256,6 +256,44 @@ pub fn apply_unop(x: Option<Literal>, sym: UnaryOperator) -> Literal {
     return output;
 }
 
+/***************************************************************************************************
+* Scan out declarations: Adapted from oxido-lang and ec-evaluator
+***************************************************************************************************/
+fn scan_out_declarations(stmts: &Vec<Stmt>) -> Vec<String> {
+    let scanned_stmt = |stmt: &Stmt| match stmt {
+        Stmt::LetStmt { name, .. } | Stmt::FuncDeclaration { name, .. } => {
+            let identifier = get_name(name);
+            vec![identifier]
+        }
+        _ => vec![]
+    };
+
+    stmts.iter()
+        .map(scanned_stmt)
+        .fold(vec![], |accumulator, element| { // fold is used to convert a collection of items into a single set
+            let mut identifiers = accumulator;
+            identifiers.extend(element);
+            identifiers
+        })
+}
+/***************************************************************************************************
+* Blocks
+* Adapted from oxido-lang https://github.com/cs4215-seville/oxido-lang/blob/main/src/compiler.rs
+***************************************************************************************************/
+fn scan_out_block_declarations(block: &Block) -> Vec<String> {
+    let block_stmts: Vec<Stmt> = block.statements
+        .iter()
+        .fold(vec![], |mut accumulate_stmts, seq_stmt| {
+            match seq_stmt {
+                SequenceStmt::Stmt(stmt) => {
+                    accumulate_stmts.push(stmt.clone());
+                    accumulate_stmts
+                },
+                _ => accumulate_stmts
+            }
+        });
+    scan_out_declarations(&block_stmts)
+}
 
 /***************************************************************************************************
 * Get the identifier name
@@ -301,8 +339,11 @@ impl Evaluate for AgendaInstrs {
                 match instr {
                     Instructions::Reset => {
                         match instr_stack.pop() {
-                            Some(AgendaInstrs::Instructions(Instructions::Mark(m)) =>
-                        };
+                            Some(AgendaInstrs::Instructions(Instructions::Mark(m))) => {
+                                // TODO: pop_environment and mutate the current environment
+                            }, // Mark found?
+                            _ => instr_stack.push(AgendaInstrs::Instructions(Instructions::Reset)) // Continue loop by pushing reset back onto agenda
+                        }
                     },
                     Instructions::Assignment(assn) => {
                             let a = Assignment_i {
@@ -332,15 +373,23 @@ impl Evaluate for AgendaInstrs {
                     Instructions::App => {},
                     Instructions::Branch => {},
                     Instructions::Mark(m) => {
-
+                        // TODO: pop_environment and mutate the current environment
                     },
                     Instructions::Assignment_i(assn) => {
                         let v = match stash.peek() {
                             Some(value) => value.clone(),
                             None => panic!("Why is nothing in the stash??")
                         };
-                        // Assign the value to the name in the environment TODO
-                        // println!("{:#?}", v);
+                        // Assign the value to the name in the environment
+                        let nam = assn.clone().sym;
+                        match env.store.get(&nam) { // Get index of where it is stored in the pool
+                            Some(val) => {
+                                env.pool.insert(*val as usize, v); // Assign value to variable
+                            }
+                            None => {
+                                panic!("Variable not initialised in the environment!");
+                            }
+                        }
                     }
                     _ => println!("Nothing is here")
                 }
@@ -357,7 +406,7 @@ impl Evaluate for Stmt {
             Stmt::LetStmt {name, is_mutable, annotation, value, position} => match value {
                 Some(expr) => {
                     let name = get_name(name);
-                    // TODO: Put name in the environment
+                    // TODO: Put name in the environment (?)
                     instr_stack.push(AgendaInstrs::Literal(Literal::UnitLiteral));
                     instr_stack.push(AgendaInstrs::Instructions(Instructions::Pop));
                     let a = Assignment {
@@ -408,8 +457,22 @@ impl Evaluate for Block {
         };
         instr_stack.push(AgendaInstrs::Instructions(Instructions::Mark(old_env))); // Drop mark on agenda
 
-        //let locals = scan_out_block_declarations(self);
-        // TODO: Locals into the environment, as unassigned
+        let mut locals = scan_out_block_declarations(self);
+        let mut curr_local: Option<String> = locals.pop();
+        while curr_local.is_some() {
+            // Overwrite current env values if there are similarly named variables
+            let curr: String = curr_local.expect("No locals");
+            match env.store.get(&curr) { // Get index of where it is stored in the pool
+                Some(val) => { // If the map points to some memory in the pool already
+                    env.pool.insert(*val as usize, Literal::UnitLiteral); // Unassigned for now
+                }
+                None => { // Else assign a new spot in the pool and then map the identifier to that
+                    env.pool.push(Literal::UnitLiteral);
+                    env.store.insert(curr, (env.pool.len() - 1) as u64);
+                }
+            }
+           curr_local = locals.pop();
+        }
 
         let mut statements_clone = self.statements.clone();
         let mut curr_stmt: Option<SequenceStmt> = statements_clone.pop();
@@ -432,9 +495,22 @@ impl Evaluate for Expr {
     fn evaluate(&self, instr_stack: &mut Vec<AgendaInstrs>, stash: &mut Vec<Literal>, env: &mut Environment) {
         match self {
             Expr::IdentifierExpr(name, source_location) => {
-                // Find the identifier in the environment
-                // Push the identifier onto the stash. (Placeholder for now)
-                stash.push(Literal::IntLiteral(5)); // TODO: Complete this.
+                match env.store.get(name) { // Find identifier in the environment
+                    Some(idx) => {
+                        let value = match env.pool.get((*idx) as usize) { // Find identifier in pool
+                            Some(lit) => {
+                                lit.clone()
+                            }
+                            None => {
+                                panic!("evaluate identifier expr: Identifier is not in pool!");
+                            }
+                        };
+                        stash.push(value);
+                    }
+                    None => {
+                        panic!("evaluate identifier expr: Identifier not found in environment!");
+                    }
+                }
             }
             Expr::LiteralExpr(literal_value, source_location) => {
                 literal_value.evaluate(instr_stack, stash, env);
