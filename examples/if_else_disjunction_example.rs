89d58268a82efb58e067f96f755cfe44e66d1eca
flsqqq <22489162+flsqqq@users.noreply.github.com>
2023-04-08 01:39:13 +0800

    add disjunction example

---
+++ examples/if_else_disjunction_example.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = 1;
+    let y = 2;
+    let a = 3;
+    let b = 4;
+    if (x == 99 && y == 99) || (a == 3 && b == 4) {
+        println("one or more sets is a match");
+    } else {
+        println("all values not a match");
+    }
+}
